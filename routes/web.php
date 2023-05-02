c148f97d39770de60b381b0899949f366305a8e5
Rishni <ashameemeduma@gmail.com>
2023-05-03 01:53:55 +0530

    created dashboard route

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/dashboard', [App\Http\Controllers\NewsController::class, 'index'])->name('news');
